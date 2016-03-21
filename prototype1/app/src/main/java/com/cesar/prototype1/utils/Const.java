a1f14d6876c21b2da2f5df81516e6f882031125f
CesarZaitoun <cezardmc@gmail.com>
2016-03-21 17:22:37 +0200

    gfu

---
+++ prototype1/app/src/main/java/com/cesar/prototype1/utils/Const.java
@@ -0,0 +1,12 @@
+package com.cesar.prototype1.utils;
+
+/**
+ * Created by ASUS on 3/21/2016.
+ */
+public class Const
+{
+
+    /** The Constant EXTRA_DATA. */
+    public static final String EXTRA_DATA = "extraData";
+}
+
